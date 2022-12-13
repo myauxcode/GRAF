8fdedd63c950f675567b201b7ca2ca33588e6db7
myauxcode <104797580+myauxcode@users.noreply.github.com>
2022-12-13 19:24:53 -0400

    Add files via upload

---
+++ PruebaG/PruebaG/Form1.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace Prueba
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.mENUToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.eJERCICIO1ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pREGUNAT2ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(204, 359);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 13;
+            this.button2.Text = "MARCO";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(321, 359);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "BORRAR";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(201, 107);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(400, 200);
+            this.pictureBox1.TabIndex = 11;
+            this.pictureBox1.TabStop = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(201, 51);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 10;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mENUToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(697, 24);
+            this.menuStrip1.TabIndex = 14;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // mENUToolStripMenuItem
+            // 
+            this.mENUToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.eJERCICIO1ToolStripMenuItem,
+            this.pREGUNAT2ToolStripMenuItem});
+            this.mENUToolStripMenuItem.Name = "mENUToolStripMenuItem";
+            this.mENUToolStripMenuItem.Size = new System.Drawing.Size(53, 20);
+            this.mENUToolStripMenuItem.Text = "MENU";
+            // 
+            // eJERCICIO1ToolStripMenuItem
+            // 
+            this.eJERCICIO1ToolStripMenuItem.Name = "eJERCICIO1ToolStripMenuItem";
+            this.eJERCICIO1ToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.eJERCICIO1ToolStripMenuItem.Text = "PREGUNTA 1";
+            this.eJERCICIO1ToolStripMenuItem.Click += new System.EventHandler(this.eJERCICIO1ToolStripMenuItem_Click);
+            // 
+            // pREGUNAT2ToolStripMenuItem
+            // 
+            this.pREGUNAT2ToolStripMenuItem.Name = "pREGUNAT2ToolStripMenuItem";
+            this.pREGUNAT2ToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.pREGUNAT2ToolStripMenuItem.Text = "PREGUNTA 2";
+            this.pREGUNAT2ToolStripMenuItem.Click += new System.EventHandler(this.pREGUNAT2ToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(697, 422);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem mENUToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem eJERCICIO1ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem pREGUNAT2ToolStripMenuItem;
+    }
+}
+
---
+++ PruebaG/PruebaG/Form1.cs
@@ -0,0 +1,262 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Prueba
+{
+    public partial class Form1 : Form
+    {
+        Graphics g;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            g = pictureBox1.CreateGraphics();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            g.DrawRectangle(Pens.Black, 0, 0, pictureBox1.Width - 1, pictureBox1.Height - 1);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Refresh();
+        }
+
+        public void ejercicio1(int n)
+        {
+            float x1, y1, x2, y2;
+            float vi1, r1, vi2, my, r2;
+
+            vi1 = 0;
+            r1 = pictureBox1.Width / n;
+            vi2 = 0;
+            my = pictureBox1.Height / 2;
+            r2 = my / n;
+
+            y1 = pictureBox1.Height / 2;
+            x2 = pictureBox1.Width;
+
+            g.DrawLine(Pens.Blue, 0, my, pictureBox1.Width, my);
+
+
+            for (int i = 1; i <= n; i++)
+            {
+
+                x1 = vi1 + (i - 1) * r1;
+                y2 = vi2 + (i - 1) * r2;
+
+                g.DrawLine(Pens.Blue, x1, y1, x2, y2);
+
+            }
+
+
+        }
+
+        public void ejercicio1_1(int n)
+        {
+            float x1, y1, x2, y2;
+            float vi1, r1, vi2, my, r2;
+
+            vi1 = 0;
+            r1 = pictureBox1.Width / n;
+            my = pictureBox1.Height / 2;
+            vi2 = my;
+            r2 = my / n;
+
+            y1 = pictureBox1.Height;
+
+            x2 = pictureBox1.Width;
+
+            g.DrawLine(Pens.Blue, 0, my, pictureBox1.Width, my);
+            for (int i = 1; i <= n; i++)
+            {
+
+                x1 = vi1 + (i - 1) * r1;
+                y2 = vi2 + (i - 1) * r2;
+
+                g.DrawLine(Pens.Blue, x1, y1, x2, y2);
+            }
+
+
+        }
+
+
+        //PREGUNTA 1
+
+
+        public void ejercicioE1(int n)
+        {
+            float x1, y1, x2, y2;
+            float vi1, r1, vi2, my, r2,mx;
+
+            vi1 = 0;
+            my = pictureBox1.Height / 2;
+
+            r1 = my / n;
+
+            mx = pictureBox1.Width / 2;
+
+            vi2 = mx;
+
+            r2 = mx / n;
+
+            x1 = 0;
+
+            y2 = my;
+
+            g.DrawLine(Pens.Blue, mx,0,mx, pictureBox1.Height);
+            g.DrawLine(Pens.Blue, 0, my, pictureBox1.Width,my);
+
+
+
+            for (int i = 1; i <= n; i++)
+            {
+
+                y1 = vi1 + (i - 1) * r1;
+
+                x2 = vi2 + (i - 1) * (-r2);
+
+
+                g.DrawLine(Pens.Blue, x1, y1, x2, y2);
+
+            }
+
+
+        }
+
+        public void ejercicioE1_1(int n)
+        {
+            float x1, y1, x2, y2;
+            float vi1, r1, vi2, my, r2, mx;
+
+            vi1 = 0;
+            my = pictureBox1.Height / 2;
+
+            r1 = my / n;
+
+            mx = pictureBox1.Width / 2;
+
+            vi2 = mx;
+
+            r2 = mx / n;
+
+            x1 = pictureBox1.Width;
+
+            y2 = my;
+
+            g.DrawLine(Pens.Blue, mx, 0, mx, pictureBox1.Height);
+            g.DrawLine(Pens.Blue, 0, my, pictureBox1.Width, my);
+
+
+
+            for (int i = 1; i <= n; i++)
+            {
+
+                y1 = vi1 + (i - 1) * r1;
+
+                x2 = vi2 + (i - 1) * (r2);
+
+
+                g.DrawLine(Pens.Blue, x1, y1, x2, y2);
+
+            }
+
+
+        }
+
+        private void eJERCICIO1ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //ejercicio1(int.Parse(textBox1.Text));
+            //ejercicio1_1(int.Parse(textBox1.Text));
+            ejercicioE1(int.Parse(textBox1.Text));
+            ejercicioE1_1(int.Parse(textBox1.Text));
+        }
+
+
+        //PREGUNTA 2
+
+
+
+        public void ejercicioE2(int n)
+        {
+            float x1, y1, x2, y2;
+            float vi1, r1, vi2, my, r2, mx, mmy, mmx;
+            Random r = new Random(); //objeto random
+
+            mx = pictureBox1.Width / 2;
+            my = pictureBox1.Height / 2;
+
+            mmy = my / 2; 
+             mmx= mx/2;
+
+            x1 = mmx;
+            y1 = mmy;
+
+            g.DrawLine(Pens.Red, 0, mmy, pictureBox1.Width, mmy);
+            g.DrawLine(Pens.Blue, mx, 0, mx, pictureBox1.Height);
+            g.DrawLine(Pens.Blue, 0, my, pictureBox1.Width, my);
+
+            for (int i = 1; i <= n; i++)
+            {
+
+                x2 = r.Next(0, (int)mx);
+                y2 = r.Next((int)mmy, (int)my);
+
+
+                g.DrawLine(Pens.Blue, x1, y1, x2, y2);
+
+            }
+
+
+        }
+
+        public void ejercicioE2_2(int n)
+        {
+            float x1, y1, x2, y2;
+            float vi1, r1, vi2, my, r2, mx, mmy, mmx;
+            Random r = new Random(); //objeto random
+
+            mx = pictureBox1.Width / 2;
+            my = pictureBox1.Height / 2;
+
+            mmy = my / 2;
+            mmx = mx / 2;
+
+            x1 = mx+mmx;
+            y1 = mmy;
+
+           
+
+            for (int i = 1; i <= n; i++)
+            {
+
+                x2 = r.Next((int)mx, (int)pictureBox1.Width);
+                y2 = r.Next(0, (int)mmy);
+
+
+                g.DrawLine(Pens.Blue, x1, y1, x2, y2);
+
+            }
+
+
+        }
+
+
+        private void pREGUNAT2ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ejercicioE2(int.Parse(textBox1.Text));
+            ejercicioE2_2(int.Parse(textBox1.Text));
+        }
+    }
+}
